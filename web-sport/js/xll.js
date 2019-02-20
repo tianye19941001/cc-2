f23cef134a0a32b04c029023597a7ff1b778a5d3
tianye1 <838199892@qq.com>
2019-02-20 10:33:38 +0800

    new H5

---
+++ web-sport/js/xll.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+	// 公共方法
+	var publicFuc = {
+		stophref: function(e){
+			if ( e && e.preventDefault ){
+				e.preventDefault(); 
+			}else{
+				window.event.returnValue = false; 
+				return false;
+			}
+		},
+		stopbubble: function(e){
+			if ( e && e.stopPropagation ){
+				e.stopPropagation(); 
+			}else{
+				window.event.cancelBubble = true;
+				return false;
+			}
+		},
+		movescroll: function(btn,to) {
+			$('html,body').animate({scrollTop: $(to).offset().top-60},600);
+		}
+	}
+});
