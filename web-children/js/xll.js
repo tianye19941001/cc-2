3c95da32e0e335a75c3c02b2eb1136d51c56f9fb
tianye1 <838199892@qq.com>
2019-03-22 18:37:21 +0800

    new

---
+++ web-children/js/xll.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+	// 公共方法
+	var publicFuc = {
+		stophref: function(e){
+			if ( e && e.preventDefault ){
+				e.preventDefault(); 
+			}else{
+				window.event.returnValue = false; 
+				return false;
+			}
+		},
+		stopbubble: function(e){
+			if ( e && e.stopPropagation ){
+				e.stopPropagation(); 
+			}else{
+				window.event.cancelBubble = true;
+				return false;
+			}
+		},
+		movescroll: function(btn,to) {
+			$('html,body').animate({scrollTop: $(to).offset().top-60},600);
+		}
+	}
+
+
+	$('.third-tit').each(function(){
+		var text = $(this).text();
+		$(this).html(text + '<span>' + text +'</span>');
+	})
+
+	$('.case>span').click(function(){
+		$(this).parent('.case').toggleClass('open')
+	})
+
+	$('.open-why').click(function(){
+		$(this).parent('.hide-why').toggleClass('open')
+	})
+});
